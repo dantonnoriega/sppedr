027a34cb9a0201a0160663d300cff713dae25a5e
ultinomics <danton.noriega@gmail.com>
2017-06-27 09:57:43 -0400

    personnel function

--- R/cps-data.R
+++ R/cps-data.R
@@ -1,18 +1,18 @@
 #' get chicago public school data
 #' @import data.table
 #' @importFrom magrittr %>%
-#' @param raw_data_dir path to raw data folder.
+#' @param data_dir path to data folder.
 #' @param force force a full import.#' @export
 
-get_cps_address_data <- function(raw_data_dir = "~/Dropbox/ra-work/spped/Data/", force = FALSE) {
+get_cps_address_data <- function(data_dir = "~/Dropbox/ra-work/spped/Data/", force = FALSE) {
 
-  # raw_data_dir = "~/Dropbox/ra-work/spped/Data/"
-  # cat(sprintf("DEV LINE: raw_data_dir = %s", raw_data_dir))
+  # data_dir = "~/Dropbox/ra-work/spped/Data/"
+  # cat(sprintf("DEV LINE: data_dir = %s", data_dir))
   if(force) {
 
-    stopifnot(dir.exists(raw_data_dir))
-    raw_data_dir <- normalizePath(raw_data_dir)
-    files <- list.files(raw_data_dir, full.names = TRUE)
+    stopifnot(dir.exists(data_dir))
+    data_dir <- normalizePath(data_dir)
+    files <- list.files(data_dir, full.names = TRUE)
 
     is_addr <- grepl('address', basename(files))
 
@@ -59,7 +59,7 @@ get_cps_address_data <- function(raw_data_dir = "~/Dropbox/ra-work/spped/Data/",
       load('data/cps_address.rda')
       return(cps_address)
     } else {
-      get_cps_address_data(raw_data_dir, force = TRUE)
+      get_cps_address_data(data_dir, force = TRUE)
     }
   }
 
@@ -69,19 +69,19 @@ get_cps_address_data <- function(raw_data_dir = "~/Dropbox/ra-work/spped/Data/",
 #' @inheretParams
 #' @export
 
-get_cps_data <- function(raw_data_dir = "~/Dropbox/ra-work/spped/Data/", force = FALSE) {
+get_cps_2008_2016 <- function(data_dir = "~/Dropbox/ra-work/spped/Data/", force = FALSE) {
 
   if(force) {
 
-    stopifnot(dir.exists(raw_data_dir))
-    raw_data_dir <- normalizePath(raw_data_dir)
-    files <- list.files(raw_data_dir, full.names = TRUE)
+    stopifnot(dir.exists(data_dir))
+    data_dir <- normalizePath(data_dir)
+    files <- list.files(data_dir, full.names = TRUE)
 
     # tag non address data
     indx <- !grepl('address', basename(files))
 
     # get school address data with lat lon
-    addr <- get_cps_address_data(raw_data_dir) %>%
+    addr <- get_cps_address_data(data_dir) %>%
       dplyr::select(schoolidr_c_d_t_s, year, address, lat, lon, address_full)
 
     cps_2008_2016 <- haven::read_dta(files[indx]) %>%
@@ -101,10 +101,54 @@ get_cps_data <- function(raw_data_dir = "~/Dropbox/ra-work/spped/Data/", force =
       load('data/cps_2008_2016.rda')
       return(cps_2008_2016)
     } else {
-      get_cps_data(raw_data_dir, force = TRUE)
+      get_cps_2008_2016(data_dir, force = TRUE)
     }
   }
 
 }
+
+#' get chicago public school data
+#' @inheretParams
+#' @export
+
+get_cps_personnel <- function(data_dir = "~/Dropbox/ra-work/spped/RawData/CPS_Personnel/cooked", force = FALSE) {
+
+  if(force) {
+
+    stopifnot(dir.exists(data_dir))
+    data_dir <- normalizePath(data_dir)
+    files <- list.files(data_dir, full.names = TRUE)
+
+    # txt and csv
+    txt_files <- files[grepl('\\.txt', basename(files))]
+    csv_files <- files[grepl('\\.csv', basename(files))]
+
+    # parse txt
+    txt_cols <- c('position_number', 'budget_category', 'unit_number', 'other')
+    txt <- txt_files %>%
+      purrr::map(.x = ., .f = readr::read_delim, delim = "|", col_names = txt_cols, trim_ws = TRUE)
+
+    # parse csv
+    csv_cols <- c("position_number","unit_number","unit_name","fte","annual_salary","fte_annual_salary","annual_benefit_cost","job_code","job_description","employee_name")
+    csv <- csv_files %>%
+      purrr::map(.x = ., .f = readr::read_csv, col_names = csv_cols, trim_ws = TRUE)
+
+    return(cps_2008_2016)
+
+  } else {
+    # check for data
+    rda <- 'data/cps_2008_2016.rda'
+
+    # if exists, load, otherwise, import using force
+    if(file.exists('data/cps_2008_2016.rda')) {
+      load('data/cps_2008_2016.rda')
+      return(cps_2008_2016)
+    } else {
+      get_cps_personnel(data_dir, force = TRUE)
+    }
+  }
+
+}
+
 
 
